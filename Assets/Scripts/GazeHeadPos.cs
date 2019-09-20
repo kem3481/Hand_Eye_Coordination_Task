61909764a1208d2971b1cc7d94e9b7ea2460d790
kem3481 <46720571+kem3481@users.noreply.github.com>
2019-09-20 16:20:49 -0400

    Update GazeHeadPos.cs

--- Assets/Scripts/GazeHeadPos.cs
+++ Assets/Scripts/GazeHeadPos.cs
@@ -2,18 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GazeHeadPos : MonoBehaviour
+namespace ViveSR
 {
-
-    // Start is called before the first frame update
-    void Start()
+    namespace anipal
     {
-        
-    }
+        namespace Eye
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            public class SR_GazeGetter : MonoBehaviour
+            {
+                private static EyeData eyeData;
+                private static VerboseData verboseData;
+                
+                public Vector3 binocularEIHorigin, binocularEIHdirection;
+
+                void Update()
+                {
+                    if (SRanipal_Eye_Framework.Status == SRanipal_Eye_Framework.FrameworkStatus.WORKING)
+                    {
+                        VerboseData data;
+                        if (SRanipal_Eye.GetVerboseData(out data) &&
+                            data.left.GetValidity(SingleEyeDataValidity.SINGLE_EYE_DATA_GAZE_DIRECTION_VALIDITY) &&
+                            data.right.GetValidity(SingleEyeDataValidity.SINGLE_EYE_DATA_GAZE_DIRECTION_VALIDITY)
+                            )
+                        {
+                            SRanipal_Eye.GetEyeData(ref eyeData);
+                            SRanipal_Eye.GetVerboseData(out verboseData);
+                            SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out binocularEIHorigin, out binocularEIHdirection);
+
+                        }
+                    }
+
+                }
+
+            }
+        }
     }
 }
